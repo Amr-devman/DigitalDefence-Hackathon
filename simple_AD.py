4ea781edd3486e88f1fde6536c92f33e47a83ec3
Ubuntu <amr@Workstation.lwn00f4qdwiexhhkzmkrksfo2d.ix.internal.cloudapp.net>
2020-11-22 01:01:08 +0000

    progress

--- simple_AD.py
+++ simple_AD.py
@@ -4,45 +4,28 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import zscore
 from sklearn.ensemble import IsolationForest
+import pickle
 
-faccount_df = pd.read_csv("./dataset/faccount.txt")
-faccount_df['FULLNAME'] = faccount_df['FIRSTNAME']+" "+faccount_df['SURNAME']
+joined_features = pd.read_csv("./dataset/joined_features.txt")
 
-
-ftxn2_df = pd.read_csv("./dataset/ftxn2.txt")
-ftxn2_df = ftxn2_df.drop(columns=[
-                                    "TXN_ID",
-                                    "ISFRAUD",
-                                    "ISFLAGGED",
-                                    "REFERENCE",
-                                    "TXTYPE",
-                                    "TXDATE"]
-                        )
-
-def id_to_name(id,id_name_dict):
-    return id_name_dict["FULLNAME"][int(id)]
+joined_features = joined_features.drop(columns={"FROMACCTID", "TOACCTID", "timestamp_normed"})
 
 
+print(joined_features.head())
 
-id_and_names = faccount_df[["ACCTID","FULLNAME"]].set_index("ACCTID").to_dict()
-
-
-# ftxn2_df["FROMNAME"] = np.vectorize(id_to_name)(ftxn2_df["FROMACCTID"], id_and_names)
-# ftxn2_df["TONAME"] = np.vectorize(id_to_name)(ftxn2_df["TOACCTID"], id_and_names)
-
-
-print(ftxn2_df.columns)
-model = IsolationForest(n_estimators=100, 
-                        max_samples=len(ftxn2_df),
+model = IsolationForest(n_estimators=200, 
+                        max_samples=len(joined_features),
                         random_state=42,
                         n_jobs=-1,
-                        verbose=1)
+                        verbose=2)
 
-predictions = model.fit_predict(ftxn2_df)
+predictions = model.fit_predict(joined_features)
 predictions_str = ['Y' if i == -1 else 'N' for i in predictions] 
-ftxn2_df["ISFRAUD"] = predictions_str
+joined_features["ISFRAUD"] = predictions_str
 
-ftxn2_df.to_csv("./dataset/ftxn2_predicted.txt", index=False)
+joined_features.to_csv("./dataset/ftxn2_predicted.txt", index=False)
 
 
+with open('isolation_forest_model','wb') as f:
+    pickle.dump(model,f)
 
